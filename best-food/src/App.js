013af48027447afa4cf736d9367e78c2aae6b407
React <3809164@gmail.com>
2023-04-01 21:29:07 +0300

    createApp-finish

--- best-food/src/App.js
+++ best-food/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import HeadlineCards from "./Components/HeadlineCards";
+import Food from "./Components/Food";
+import Category from "./Components/Category";
 
 function App() {
-  return (
-   <div>
-
-   </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <Hero/>
+            <HeadlineCards/>
+            <Food/>
+            <Category/>
+        </div>
+    );
 }
 
 export default App;
